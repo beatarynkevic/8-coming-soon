f3e81fe9c0f37825ba8a94b21889f8d1f2adccff
beatarynkevic <beatainn@gmail.com>
2020-11-27 12:26:13 +0200

    Clock

--- js/components/socials/isValidSocialItem.js
+++ js/components/socials/isValidSocialItem.js
@@ -3,8 +3,6 @@
  * @param {object} itemObject Objektas aprasantis viena social nuoroda, kuria sudaro ikona ir nuoroda (href)
  * @returns {boolean} Jei patikrinimo metu randama logine duomenu klaida, tai grazina `false`, priesingu atveju - `true`
  */
-
-
 function isValidSocialItem(itemObject) {
     if (typeof itemObject !== 'object') {
         console.warn('Error: social elemento turetubuti objektas.');
