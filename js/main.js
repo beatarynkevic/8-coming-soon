bea21c869a8a7faaf3b71fef842ec0b338b53abf
beatarynkevic <beatainn@gmail.com>
2020-11-28 14:54:04 +0200

    Clock rendering

--- js/main.js
+++ js/main.js
@@ -1,4 +1,7 @@
 import { renderSocials } from './components/socials/renderSocials.js'; //ikeliu funkcija
 import { socialsData } from './data/socialsData.js';           //ikeliu sarasa
+import { renderClock } from './components/clock/renderClock.js';
 
-renderSocials('footer > .row', socialsData);                  //funkcijai duodu duomeis
+renderSocials('footer > .row', socialsData);                 //funkcijai duodu duomeis
+
+renderClock('.clock');
