48817dadc6e68c0d43f687a3c9e1fe748d03777b
beatarynkevic <beatainn@gmail.com>
2020-12-01 21:02:50 +0200

    Progress bars js

---
+++ js/components/progress-bar/renderAllProgressBars.js
@@ -0,0 +1,20 @@
+import { renderProgressBar } from './renderProgressBar.js';
+import { isValidProgressBarsList } from './isValidProgressBarsList.js';
+import { isValidSingleProgressBar } from './isValidSingleProgressbar.js';
+
+function renderAllProgressBars(data) {
+    if (!isValidProgressBarsList(data)) {
+        return false;
+    }
+
+    for (let i = 0; i < data.length; i++) {
+        const bar = data[i];
+
+        if (!isValidSingleProgressBar(bar)) {
+            continue;
+        }
+        renderProgressBar(bar.selector, bar.title, bar.value);
+    }
+}
+
+export { renderAllProgressBars }
